ef3cc595e5babd367355aae3f0a6728a5f7ba97b
rgv250cc <roman.grinevic@gmail.com>
2024-03-31 18:48:44 +0300

    .

--- starwars-app/src/main.js
+++ starwars-app/src/main.js
@@ -1,8 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import router from './router'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
+//Vuex
 import store from './store'
+
+//Router
+import router from './router'
+
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
 createApp(App).use(store)
   .use(vuetify)
